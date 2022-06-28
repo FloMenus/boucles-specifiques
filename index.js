12cd87e37a37b7a36412f8d67750e511e4628075
FloMenus <florent.mns@gmail.com>
2022-06-28 17:10:29 +0200

    only ex. 8 to do

--- index.js
+++ index.js
@@ -144,20 +144,23 @@
 
 ////////////////////////////////////////////////////////
 
-// 7 - Factorielle ForEach (PAS FINI)
+// 7 - Factorielle ForEach
 
-// let factorielle = [0, 1, 2, 3, 4, 5, 6, 7]
+// const factorielle = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-// let factorisation = factorielle.forEach ((num, i) => {
-//     num*=i
-//     return num
+// let result = 1
+
+// factorielle.forEach((num, i) => {
+//     result*=num
 // })
 
-// // console.log (factorisation)
+// console.log (result)
+ 
 
 //////////////////////////////////////////////////////////////
 
-// 8 - Chess board (PAS FAIT)
+// 8 - Chess board (PAS FAIT)7
+
 
 //////////////////////////////////////////////////////
 
