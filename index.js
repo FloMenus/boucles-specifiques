642d98ef47ba93ab85eb50a203bea345e922de5f
FloMenus <florent.mns@gmail.com>
2022-06-28 14:32:34 +0200

    created file, started exercices

---
+++ index.js
@@ -0,0 +1,84 @@
+// 1 - Map Double
+
+// let array = [1, 2, 3, 4, 5]
+
+// let doubles = array.map((number, i) => {
+//     return number*2
+// })
+
+// console.log (doubles)
+
+// 2 - Map Names
+
+// let longNames = [
+//     {
+//         firstName: "Jane",
+//         lastName: "Doe"
+//     }
+//     ,
+//     {
+//         firstName: "John",
+//         lastName: "Smith"
+//     }
+// ]
+
+// console.table(longNames)
+
+
+
+// let shortNames = longNames.map((name, i) => {
+//     return  name = longNames.firstName + longNames.lastName
+    
+// })
+
+// console.log (shortNames)
+
+// 3 - Filter Numbers
+
+// let array = [1, "Toto", 34, "JavaScript", 8]
+
+// let number = array.filter((element, i) => {
+//     return parseInt(element)
+// })
+
+// console.log (number)
+
+// 4 - Filter Even
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+
+// let even = numbers.filter((number, i) => {
+//     return number%2 === 0
+// })
+
+// console.log (even)
+
+// 5 - Cakes
+
+let cakes = [
+	{
+		name: "cake",
+		flavor: "vanilla",
+		status: "available"
+	},
+	{
+		name: "brownie",
+		flavor: "chocolate",
+		status: "available"
+	},
+	{
+		name: "pie",
+		flavor: "strawberry",
+		status: "available"
+	},
+	{
+		name: "muffin",
+		flavor: "pistachio",
+		status: "available"
+	},
+	{
+		name: "donut",
+		flavor: "chocolate",
+		status: "available"
+	},
+]
